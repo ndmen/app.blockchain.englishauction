d5134ff8db7705e2fa50b90300646b74a8930965
Artem Abdullin <artemleelooai@gmail.com>
2022-08-08 23:50:39 +0300

    Added interface IERC721

---
+++ EnglishAuction.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+interface IERC721 {
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint tokenId
+    ) external;
+
+    function transferFrom(
+        address,
+        address,
+        uint
+    ) external;
+}
